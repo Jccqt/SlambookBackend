5c7ef8ca2d66377c02cf6454790e072877debdaf
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-06-13 09:18:21 +0800

    feat(crypt): add crypt class for hashing and salting

---
+++ SlambookBackend/Tools/Crypt.cs
@@ -0,0 +1,42 @@
+﻿using System.Security.Cryptography;
+using System.Text;
+
+namespace SlambookBackend.Tools
+{
+    public class Crypt
+    {
+        public static string HashPassword(string password, string salt)
+        {
+            using (SHA256 sha256 = SHA256.Create())
+            {
+                string saltedPassword = password + salt;
+
+                // will hash the saltedPassword into 256-bit hash
+                byte[] bytes = sha256.ComputeHash(Encoding.UTF8.GetBytes(saltedPassword));
+                StringBuilder builder = new StringBuilder();
+
+                // will converts each byte into its 2-digit lowercase hexadecimal form
+                // Example: "4b227777d4dd1fc61c6f884f48641d02..."
+                foreach (byte b in bytes)
+                {
+                    builder.Append(b.ToString("x2"));
+                }
+                return builder.ToString();
+            }
+        }
+
+        public static string GenerateSalt()
+        {
+            byte[] saltByes = new byte[16]; // will create a 16-byte array
+
+            using (var random = RandomNumberGenerator.Create())
+            {
+                random.GetBytes(saltByes); // will fills it with cryptographically random values
+            }
+
+            // will convert bytes into a Base64 Str
+            // Example: "bZf34Gv2aF+5QZz9q3bXyA=="
+            return Convert.ToBase64String(saltByes);
+        }
+    }
+}

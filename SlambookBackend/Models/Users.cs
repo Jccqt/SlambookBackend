def8057240222d5b1c8ed4ecb547da27f41fe40d
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-06-13 21:45:00 +0800

    refactor(users): update users model

--- SlambookBackend/Models/Users.cs
+++ SlambookBackend/Models/Users.cs
@@ -5,16 +5,33 @@ namespace SlambookBackend.Models
     public class Users
     {
         public int Id { get; set; }
+
+        [Column("first_name")]
         public string FirstName { get; set; } = string.Empty;
+
+        [Column("last_name")]
         public string LastName { get; set; } = string.Empty;
+
+        [Column("bio")]
         public string Bio { get; set; } = string.Empty;
+
+        [Column("username")]
         public string Username { get; set; } = string.Empty;
+
+        [Column("password")]
         public string Password { get; set; } = string.Empty;
+
+        [Column("salt")]
         public string Salt { get; set; } = string.Empty;
+
+        [Column("email")]
         public string Email { get; set; } = string.Empty;
-        [Column(TypeName = "mediumblob")]
+
+        [Column("profile_picture", TypeName = "mediumblob")]
         public byte[]? ProfilePicture { get; set; }
         public int Status { get; set; }
+
+        [Column("login_count")]
         public int LoginCount { get; set; }
     }
 }

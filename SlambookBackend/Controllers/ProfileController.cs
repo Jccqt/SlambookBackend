83581d50218657bf31ddaa338e107a7181fab26c
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-06-13 20:24:01 +0800

    feat(profile): add profile controller
    - add get all profiles endpoint.

---
+++ SlambookBackend/Controllers/ProfileController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SlambookBackend.Interfaces;
+using SlambookBackend.Models;
+
+namespace SlambookBackend.Controllers
+{
+    [Route("api/user/profile")]
+    [ApiController]
+    public class ProfileController : ControllerBase
+    {
+        private readonly IProfileRepository _profileRepo;
+
+        public ProfileController(IProfileRepository profileRepo)
+        {
+            _profileRepo = profileRepo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ServiceResponse>> GetAllProfiles([FromQuery] int count)
+        {
+            var result = await _profileRepo.GetAllProfiles(count);
+
+            return result.Success ? Ok(result) : NotFound(result);
+        }
+    }
+}

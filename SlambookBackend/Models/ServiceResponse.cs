f88c5233e51db45f4fe9bb84cd4cd8d45f54a4e7
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-06-13 09:08:36 +0800

    feat(response): add service response class

---
+++ SlambookBackend/Models/ServiceResponse.cs
@@ -0,0 +1,17 @@
+﻿namespace SlambookBackend.Models
+{
+    public class ServiceResponse<T>
+    {
+        public bool Success { get; set; } = false;
+        public string Message { get; set; } = string.Empty;
+        public T? Data { get; set; }
+        public string TraceID { get; set; } = string.Empty;
+    }
+
+    public class ServiceResponse
+    {
+        public bool Success { get; set; } = false;
+        public string Message { get; set; } = string.Empty;
+        public string TraceID { get; set; } = string.Empty;
+    }
+}

9f37f85f854c11b591061a5b33881be97dae15c7
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-06-13 09:33:08 +0800

    feat(auth): add login request dto class

---
+++ SlambookBackend/DTO/Auth/LoginRequestDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SlambookBackend.DTO.Auth
+{
+    public class LoginRequestDTO
+    {
+        [Required]
+        [EmailAddress(ErrorMessage = "Please provide a valid email format.")]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+    }
+}

12dd6b7b878a0b54f542acc5c5b5f7293776bac7
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-06-13 08:47:19 +0800

    refactor(users): update users model class

--- SlambookBackend/Models/Users.cs
+++ SlambookBackend/Models/Users.cs
@@ -3,9 +3,12 @@
     public class Users
     {
         public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
         public string Username { get; set; } = string.Empty;
         public string Password { get; set; } = string.Empty;
         public string Salt { get; set; } = string.Empty;
+        public string Email { get; set; } = string.Empty;
         public int Status { get; set; }
     }
 }

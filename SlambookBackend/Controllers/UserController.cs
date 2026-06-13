238a5670cd1abf55703e604454fa577a93861af6
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-06-13 17:19:51 +0800

    feat(users): add user controller
    - add get all users endpoint.

---
+++ SlambookBackend/Controllers/UserController.cs
@@ -0,0 +1,27 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SlambookBackend.Interfaces;
+using SlambookBackend.Models;
+
+namespace SlambookBackend.Controllers
+{
+    [Route("api/user")]
+    [ApiController]
+    public class UserController : ControllerBase
+    {
+        private readonly IUserRepository _userRepo;
+
+        public UserController(IUserRepository userRepo)
+        {
+            _userRepo = userRepo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ServiceResponse>> GetAllUsers()
+        {
+            var response = await _userRepo.GetAllUsers();
+
+            return response.Success ? Ok(response) : NotFound(response);
+        }
+    }
+}

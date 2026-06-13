a62de59015d2f4e301acec35ea87f5430cf791cf
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-06-13 17:16:55 +0800

    feat(users): add user dto class

---
+++ SlambookBackend/DTO/Users/UserDTO.cs
@@ -0,0 +1,14 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SlambookBackend.DTO.Users
+{
+    public class UserDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string Bio { get; set; } = string.Empty;
+        public string ProfilePicture { get; set; } = string.Empty;
+    }
+}

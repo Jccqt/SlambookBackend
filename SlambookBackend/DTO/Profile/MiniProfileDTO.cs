a543b523cd2b7c6c79b5c46f098e56e58381e4be
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-06-13 20:21:09 +0800

    feat(profile): add mini profile dto

---
+++ SlambookBackend/DTO/Profile/MiniProfileDTO.cs
@@ -0,0 +1,11 @@
+﻿namespace SlambookBackend.DTO.Profile
+{
+    public class MiniProfileDTO
+    {
+        public int Id { get; set; }
+        public string FirstName { get; set; } = string.Empty;
+        public string LastName { get; set; } = string.Empty;
+        public string Username { get; set; } = string.Empty;
+        public string ProfilePicture { get; set; } = string.Empty;
+    }
+}

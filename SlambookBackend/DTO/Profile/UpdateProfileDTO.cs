3f1209ffb964eae2d4a0ec62234c8d909f5cab57
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-06-14 10:19:22 +0800

    feat(profile): add update-profile dto

---
+++ SlambookBackend/DTO/Profile/UpdateProfileDTO.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel.DataAnnotations;
+using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SlambookBackend.DTO.Profile
+{
+    public class UpdateProfileDTO
+    {
+        [Required]
+        public string Username { get; set; }
+
+        [Required]
+        public string Bio { get; set; }
+
+        [Required]
+        public IFormFile ProfilePicture { get; set; }
+    }
+}

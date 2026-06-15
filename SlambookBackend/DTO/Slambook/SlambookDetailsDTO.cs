c4ff27a986ced240549d1f3bd8d22f322d046247
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-06-15 19:15:03 +0800

    feat(slambook): add slambook details dto

---
+++ SlambookBackend/DTO/Slambook/SlambookDetailsDTO.cs
@@ -0,0 +1,14 @@
+﻿using SlambookBackend.DTO.Profile;
+
+namespace SlambookBackend.DTO.Slambook
+{
+    public class SlambookDetailsDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public string Description { get; set; } = string.Empty;
+        public DateOnly CreatedDate { get; set; }
+
+        public ICollection<MiniProfileDTO> Responses = new List<MiniProfileDTO>();
+    }
+}

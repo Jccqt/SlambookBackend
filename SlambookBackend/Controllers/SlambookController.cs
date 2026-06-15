269ee3ff084f63a5ef560b34d88bdf696e2a2d86
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-06-15 18:34:56 +0800

    feat(slambook): add slambook controller
    - add get-all slambooks endpoint

---
+++ SlambookBackend/Controllers/SlambookController.cs
@@ -0,0 +1,34 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+using SlambookBackend.Interfaces;
+using SlambookBackend.Models;
+
+namespace SlambookBackend.Controllers
+{
+    [Route("api/user/{userId}/slambook")]
+    [ApiController]
+    public class SlambookController : ControllerBase
+    {
+        private readonly ISlambookRepository _slambookRepo;
+
+        public SlambookController(ISlambookRepository slambookRepo)
+        {
+            _slambookRepo = slambookRepo;
+        }
+
+        [HttpGet]
+        public async Task<ActionResult<ServiceResponse>> GetAllSlambooks(
+            [FromRoute] int userId,
+            [FromQuery] int count)
+        {
+            if(userId <= 0)
+            {
+                return BadRequest(new ServiceResponse { Message = "Invalid user ID." });
+            }
+
+            var results = await _slambookRepo.GetAllSlambooks(count);
+
+            return results.Success ? Ok(results) : NotFound(results);
+        }
+    }
+}

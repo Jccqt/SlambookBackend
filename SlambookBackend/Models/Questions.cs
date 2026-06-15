ac247628091d0c763dde3e7015014d155f6c3a1b
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-06-15 09:31:01 +0800

    feat(question): add querstions model

---
+++ SlambookBackend/Models/Questions.cs
@@ -0,0 +1,15 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SlambookBackend.Models
+{
+    public class Questions
+    {
+        public int Id { get; set; }
+
+        [Column("slambook_id")]
+        public int SlambookId { get; set; }
+
+        [Column("question_text")]
+        public string QuestionText { get; set; } = string.Empty;
+    }
+}

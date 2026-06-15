642846fd818b4feb5e14d0e49f89288c59e01abb
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-06-15 12:42:11 +0800

    feat(slambook): add slambook dto

---
+++ SlambookBackend/DTO/Slambook/SlambookDTO.cs
@@ -0,0 +1,9 @@
+﻿namespace SlambookBackend.DTO.Slambook
+{
+    public class SlambookDTO
+    {
+        public int Id { get; set; }
+        public string Title { get; set; } = string.Empty;
+        public int ResponseCount { get; set; }
+    }
+}

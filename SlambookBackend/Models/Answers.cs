9f49771f1d82eab6bc33ce7a4662ee762ba4d87c
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-06-15 09:32:27 +0800

    feat(answers): add answers model

---
+++ SlambookBackend/Models/Answers.cs
@@ -0,0 +1,18 @@
+﻿using System.ComponentModel.DataAnnotations.Schema;
+
+namespace SlambookBackend.Models
+{
+    public class Answers
+    {
+        public int Id { get; set; }
+
+        [Column("question_id")]
+        public int QuestionId { get; set; }
+
+        [Column("responder_id")]
+        public int ResponderId { get; set; }
+
+        [Column("answer_text")]
+        public string AnswerText { get; set; } = string.Empty;
+    }
+}

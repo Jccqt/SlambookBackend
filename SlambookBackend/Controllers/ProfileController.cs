c56ffbe9ce6707fa5b447eb2f0ece154a811e9f4
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-06-16 18:23:39 +0800

    docs(profile): document endpoint summaries

--- SlambookBackend/Controllers/ProfileController.cs
+++ SlambookBackend/Controllers/ProfileController.cs
@@ -17,6 +17,9 @@ namespace SlambookBackend.Controllers
             _profileRepo = profileRepo;
         }
 
+        /// <summary>
+        /// Retrieves all mini profiles that can be used in homepage and members page.
+        /// </summary>
         [HttpGet]
         public async Task<ActionResult<ServiceResponse>> GetAllProfiles([FromQuery] int count)
         {
@@ -25,14 +28,20 @@ namespace SlambookBackend.Controllers
             return result.Success ? Ok(result) : NotFound(result);
         }
 
+        /// <summary>
+        /// Retrieves a mini profile using a username. This can be used in members page.
+        /// </summary>
         [HttpGet("by-username/{username}")]
-        public async Task<ActionResult<ServiceResponse>> GetProfileByUsername([FromQuery] string username)
+        public async Task<ActionResult<ServiceResponse>> GetProfileByUsername([FromRoute] string username)
         {
             var result = await _profileRepo.GetProfileByUsername(username);
 
             return result.Success ? Ok(result) : NotFound(result);
         }
 
+        /// <summary>
+        /// Retrieves the profile picture bytes that can be used by Glide or any other extension packages.
+        /// </summary>
         [HttpGet("{userId}/profile-picture")]
         public async Task<ActionResult<ServiceResponse>> GetProfilePictureBytes([FromRoute] int userId)
         {
@@ -45,6 +54,10 @@ namespace SlambookBackend.Controllers
 
             return result.Success ? Ok(result) : NotFound(result);
         }
+
+        /// <summary>
+        /// Updates the profile details. This should be in user controller, however, I separated this because of the profile picture.
+        /// </summary>
 
         [HttpPatch("update/{userId}")]
         public async Task<ActionResult<ServiceResponse>> UpdateProfile([FromRoute] int userId, [FromForm] UpdateProfileDTO profile)

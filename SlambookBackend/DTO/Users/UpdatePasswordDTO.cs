bb9f639fbf9eb2d2f7c88784f0bc034145eec13e
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-06-16 20:26:51 +0800

    feat(user): add update-password dto

---
+++ SlambookBackend/DTO/Users/UpdatePasswordDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SlambookBackend.DTO.Users
+{
+    public class UpdatePasswordDTO
+    {
+        [Required]
+        public string OldPassword { get; set; }
+
+        [Required]
+        public string NewPassword { get; set; }
+    }
+}

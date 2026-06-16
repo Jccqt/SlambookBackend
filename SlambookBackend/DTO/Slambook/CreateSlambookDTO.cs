8da14eafc8fb2e66abe7610c66f3fbf958f08079
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-06-16 11:24:34 +0800

    feat(slambook): add create-slambook dto

---
+++ SlambookBackend/DTO/Slambook/CreateSlambookDTO.cs
@@ -0,0 +1,19 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SlambookBackend.DTO.Slambook
+{
+    public class CreateSlambookDTO
+    {
+        [Required]
+        public int CreatorId { get; set; }
+
+        [Required]
+        public string Title { get; set; }
+
+        [Required]
+        public string Description { get; set; }
+
+        [Required]
+        public List<string> QuestionText { get; set; } 
+    }
+}

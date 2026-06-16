d2ffa792f51ab26bf1b38a54116fefad04a26222
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-06-16 15:01:35 +0800

    feat(slambook): add answer item dto and submit answers dto

---
+++ SlambookBackend/DTO/Slambook/AnswerItemDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SlambookBackend.DTO.Slambook
+{
+    public class AnswerItemDTO
+    {
+        [Required]
+        public int QuestionId { get; set; }
+
+        [Required]
+        public string AnswerText { get; set; }
+    }
+}
---
+++ SlambookBackend/DTO/Slambook/SubmitAnwersDTO.cs
@@ -0,0 +1,13 @@
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace SlambookBackend.DTO.Slambook
+{
+    public class SubmitAnwersDTO
+    {
+        [Required]
+        public int ResponderId { get; set; }
+
+        [Required]
+        public List<AnswerItemDTO> Answers { get; set; } = new List<AnswerItemDTO>(); 
+    }
+}

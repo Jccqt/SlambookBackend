3b1b2775b87c78b4eb9da3d2ff94cbe82c113fa4
Jc Calayag <138150245+Jccqt@users.noreply.github.com>
2026-07-01 18:17:05 +0800

    test(user): add UserRepositoryTests class
    Added the following tests:
    - GetAllUsers_WhenusersFound_ShouldReturnUserDTOList.

---
+++ Slambook.UnitTests/Repository/UserRepositoryTests.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xunit;
+using SlambookBackend.Repository;
+using SlambookBackend.Models;
+using Slambook.UnitTests.Helpers;
+using Slambook.UnitTests.DataGenerators;
+using SlambookBackend.DTO.Users;
+
+namespace Slambook.UnitTests.Repository
+{
+    public class UserRepositoryTests
+    {
+        private readonly UserFaker _users = new UserFaker();
+
+        [Fact]
+        public async Task GetAllUsers_WhenUsersFound_ShouldReturnUserDTOList()
+        {
+            // Arrange
+            using var context = DbContextHelper.GetInMemoryContext();
+
+            var users = _users.Generate(10);
+            context.AddRange(users);
+            await context.SaveChangesAsync();
+
+            var repository = new UserRepository(context);
+
+            // Act
+            var result = await repository.GetAllUsers(CancellationToken.None);
+
+            // Assert
+            Assert.NotNull(result);
+            Assert.NotNull(result.Data);
+
+            var returnedUsers = Assert.IsType<List<UserDTO>>(result.Data);
+
+            Assert.Equal(10, returnedUsers.Count);
+        }
+    }
+}
